d390b2c1458d4096b88210bdd0be34b2c16b19de
Utkarsh Patel <utkarshhpatel13@gmail.com>
2021-02-02 13:12:49 +0530

    Minor Changes

--- src/components/reviews/reviews.jsx
+++ src/components/reviews/reviews.jsx
@@ -9,7 +9,7 @@ class Reviews extends Component {
     render() {
         return (
             <main>
-                <section className="bg-white section text-black">
+                <section className="bg-yellow-300 section text-black">
                     <div className="title">
                         <h2>Our Reviews</h2>
                         <div className="underlinediv"></div>
